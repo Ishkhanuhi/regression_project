4fc67d06df8670a80e0e2677f52f40e5f75ce3ba
Ishkhanuhi <ishkhanuhi.hakobyan@fluxtechs.com>
2023-02-09 12:13:35 +0400

    ready

--- README.md
+++ README.md
@@ -36,4 +36,83 @@ The result from the analysis is saved into `data_with_sentiment_analysis.csv`.
 
 ## Data Analysis and Regression
 
-We're going to analyse the relation between 
+We're going to analyse the relation between video `likes` and `views`.
+![alt text](img2.png)
+
+As we can see from the scatter plot there's strong linear relation between these two variables.
+
+So, we can do linear regression to find the best linear function that describes the relation.
+
+![alt text](img3.png)
+
+Then we are viewing the desired metrics:
+
+```md
+Score: 0.9228154633523422
+Coefficients:  [[0.00955015]]
+Intercept:  [687.49364687]
+t-statistics:  92.35199654274811
+R2 score:  0.7951924316063258
+```
+
+### Coefficient
+
+Each independent variable in a linear regression model has its own coefficient. The coefficient for an independent
+variable represents the change in the dependent variable associated with a one-unit change in the independent variable,
+while holding all other independent variables constant. In other words, it represents how much the dependent variable
+changes for each unit change in the independent variable.
+
+The coefficients can be positive, negative, or zero, depending on the relationship between the independent and dependent
+variables. If the coefficient is positive, it means that as the independent variable increases, the dependent variable
+also increases. If the coefficient is negative, it means that as the independent variable increases, the dependent
+variable decreases. If the coefficient is zero, it means that the independent variable has no effect on the dependent
+variable.
+
+The magnitude of the coefficients can also provide information about the strength of the relationship between the
+independent and dependent variables. In general, larger coefficients indicate a stronger relationship.
+
+### t-statistics
+
+The t-statistics of independent variables in a regression analysis are used to assess the statistical significance of
+each independent variable in explaining the variation in the dependent variable. The t-statistic is the ratio of the
+estimated coefficient of the independent variable to its standard error.
+
+A positive t-statistic indicates that the estimated coefficient of the independent variable is positively associated
+with the dependent variable, while a negative t-statistic indicates that the estimated coefficient is negatively
+associated with the dependent variable. The magnitude of the t-statistic reflects the strength of the association. The
+larger the t-statistic, the stronger the association.
+
+The null hypothesis for the t-test is that the true coefficient of the independent variable is equal to zero, which
+means that the independent variable has no effect on the dependent variable. If the absolute value of the t-statistic is
+greater than the critical value from a t-distribution with degrees of freedom equal to the number of observations minus
+the number of independent variables, then the null hypothesis can be rejected and the independent variable is considered
+to have a statistically significant effect on the dependent variable.
+
+## Logistic Regression
+
+We are going to analyse the relation between the `views` and `sentiments`.
+
+![alt text](img4.png)
+
+![alt text](img5.png)
+
+## Decision Tree
+```md
+Accuracy: 0.53
+
+|--- views <= 1839095.50
+| |--- views <= 221717.50
+| | |--- views <= 208714.00
+| | | |--- views <= 47743.50
+| | | | |--- views <= 39947.50
+| | | | | |--- views <= 23411.00
+| | | | | | |--- class: 0
+| | | | | |--- views >  23411.00
+| | | | | | |--- views <= 28733.50
+| | | | | | | |--- views <= 26410.00
+| | | | | | | | |--- class: 2
+| | | | | | | |--- views >  26410.00
+| | | | | | | | |--- views <= 26797.00
+| | | | | | | | | |--- class: 0
+
+```
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import statsmodels.api as sm
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import label_binarize
+from sklearn.preprocessing import LabelEncoder
 
 from helpers import *
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, confusion_matrix, auc, roc_curve
-from sklearn.linear_model import LogisticRegression
-from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import r2_score, confusion_matrix
+from sklearn import tree
 
 
 def main():
@@ -23,7 +22,8 @@ def main():
 
     X = df['views'].values
     y = df['likes'].values
-
+    plt.scatter(X, y)
+    # plt.show()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     reg = LinearRegression()
@@ -31,13 +31,12 @@ def main():
     print("Score: ", reg.score(X_train.reshape((-1, 1)), y_train.reshape((-1, 1))))
     print("Coefficients: ", reg.coef_)
     print("Intercept: ", reg.intercept_)
-
+    plt.plot(X, reg.coef_[0] * X + reg.intercept_, color='r')
+    plt.show()
     y_pred = reg.predict(X_test.reshape((-1, 1)))
 
     model = sm.OLS(y_train, X_train).fit()
     slope = model.params[0]
-    n = len(X_train)
-    p = 1  # number of parameters
     t = slope / model.bse[0]
     r2 = r2_score(y_test, y_pred)
 
@@ -48,59 +47,29 @@ def main():
     clf_x = df['views'].values
     clf_y = df['sentiment'].values
 
-    clf_y = label_binarize(clf_y, classes=[0, 1, 2])
-    n_classes = 3
+    plt.bar(clf_y, clf_x)
+    plt.show()
+
+    le = LabelEncoder()
+    le.fit(clf_y)
+
+    clf_y = le.transform(clf_y)
     clf_x_train, clf_x_test, clf_y_train, clf_y_test = train_test_split(clf_x, clf_y, test_size=0.2)
-    clf = OneVsRestClassifier(LogisticRegression(random_state=42, max_iter=1000))
-    y_score = clf.fit(clf_x_train.reshape((-1, 1)), clf_y_train.ravel()).decision_function(clf_x_test)
-
-    print("Logistic Regression params: ", clf.coef_, clf.intercept_)
-    print("Classifier Score: ", clf.score(clf_x_train.reshape((-1, 1)), clf_y_train.ravel()))
-    clf_y_pred = clf.predict(clf_x_test.reshape((-1, 1)))
-    print("Pseudo r squared: ", efron_rsquare(clf_y_test, clf_y_pred))
-
+    log_model = LogisticRegression()
+    log_model.fit(clf_x_train.reshape((-1, 1)), clf_y_train)
+    clf_y_pred = log_model.predict(clf_x_test.reshape((-1, 1)))
+    print('Accuracy of Logistic Regression classifier on test set: {:.2f}'.format(
+        log_model.score(clf_x_test.reshape((-1, 1)), clf_y_test)))
     print(confusion_matrix(clf_y_test, clf_y_pred))
-    # Compute ROC curve and ROC area for each class
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(clf_y_test[:, i], y_score[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
 
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(clf_y_test.ravel(), y_score.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-    # Plot of a ROC curve for a specific class
-    plt.figure()
-    plt.plot(fpr[2], tpr[2], label='ROC curve (area = %0.2f)' % roc_auc[2])
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
-    plt.legend(loc="lower right")
-    plt.show()
-
-    # Plot ROC curve
-    plt.figure()
-    plt.plot(fpr["micro"], tpr["micro"],
-             label='micro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["micro"]))
-    for i in range(n_classes):
-        plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
-                                       ''.format(i, roc_auc[i]))
-
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Some extension of Receiver operating characteristic to multi-class')
-    plt.legend(loc="lower right")
-    plt.show()
+    clf = tree.DecisionTreeClassifier(max_depth=20)
+    clf.fit(clf_x_train.reshape((-1, 1)), clf_y_train)
+    y_pred = clf.predict(clf_x_test.reshape((-1, 1)))
+    correct_predictions = (y_pred == clf_y_test).sum()
+    accuracy = correct_predictions / len(y_test)
+    print("Accuracy:", accuracy)
+    r = tree.export_text(clf, feature_names=['views'])
+    print(r)
 
 
 # Press the green button in the gutter to run the script.
